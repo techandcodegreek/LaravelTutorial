36b655a80173d2b190e5ecf7081e2f1907604fda
Tech and code <techandcodegreek@gmail.com>
2020-10-22 15:15:52 +0300

    checkpoint

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,14 +3,15 @@
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
         </x-slot>
-
+        
         <x-jet-validation-errors class="mb-4" />
-
+        
         @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
+        <div class="mb-4 font-medium text-sm text-green-600">
+            {{ session('status') }}
+        </div>
         @endif
+        <div class="float-right"><a href="/register">Register</a></div>
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
